0e4dec41f4bdced7d87fca6084581e2dd8f9f2ac
mu-kumaran <mu.kumaran1991@gmail.com>
2025-05-16 18:02:07 +0530

    New commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+// var http = require("http")
+// http.createServer(function(req,res){
+//     res.end("<h1>Welcome to node js</h1>")
+// }).listen(9890)
+
+// console.log("Port listening at....... 9890")
+
 var http = require("http")
+const PORT = process.env.PORT || 9012
+const app = http.createServer(function(req,res){
+    res.end("<h1>Welcome to node js local to live</h1>")
+})
 
-http.createServer(function(req,res){
-    res.end("<h1>Welcome to node js</h1>")
-}).listen(9890)
+app.listen(PORT,()=>{
+    console.log(`Port listening at....... ${PORT}`)
+})
 
-console.log("Port listening at....... 9890")
